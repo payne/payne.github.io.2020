bf9aebeb92e7bab0b3a76f1556c99a2afb3ff0b3
Matt Payne <Payne@MattPayne.org>
2020-02-23 17:55:00 -0600

    Updated with my info...

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `MattPayne.org`,
+    author: `Matt Payne`,
+    description: `a bad blog...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/MattPayneOrg`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/payne`,
       },
     ],
   },
